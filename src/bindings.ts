6508820006efd13ceafdbe3c26336d367600ad26
Bogdan Manate <bogdan.manate@gmail.com>
2020-04-04 12:15:28 +0200

    Add factory function for circle and rect

--- src/bindings.ts
+++ src/bindings.ts
@@ -3,6 +3,9 @@ import {pipe} from 'fp-ts/lib/pipeable';
 import { io } from 'fp-ts/lib/IO'
 import {safeGetDOMElement, safeAddEventListener} from './dom-utils'
 
+
+//Todo: The buttons are selected by id, which will add contraints on an existing implementation.
+// 			The bind function should also work on HTMLElements 
 export const bindControlButton = (btnId:string, callback: EventListenerOrEventListenerObject) => pipe(
 	io.chain(safeGetDOMElement(btnId), (opt) => 
 			pipe(
